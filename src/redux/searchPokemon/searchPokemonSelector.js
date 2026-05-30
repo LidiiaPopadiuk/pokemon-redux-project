ed51c1af4c17c2146acd218b28b318310f88e887
LidiiaPopadiuk <lidiiadolfin@gmail.com>
2026-05-30 17:14:01 +0200

    feat: finish app

--- src/redux/searchPokemon/searchPokemonSelector.js
+++ src/redux/searchPokemon/searchPokemonSelector.js
@@ -1,5 +1,5 @@
 export const getFind = (state) => {
     return state.pokemons.pokemons.filter(pokemon => 
-        pokemon.name.toLowerCase().includes(state.filter.toLowerCase())
+        pokemon.name.toLowerCase().includes(state.filter.toLowerCase() )
     )
 }
